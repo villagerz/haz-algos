34b4d9796b2de9cd4c9e4f1a317cd0e9925bb657
Villager Z <git.villagerz@pm.me>
2020-12-13 18:34:06 -0500

    refined mx sum of segment to use max sum f prefix

--- library/Algz/Segments.hs
+++ library/Algz/Segments.hs
@@ -3,9 +3,13 @@ module Algz.Segments where
 import           Data.List       (foldr1, inits, tails)
 import           Prelude.Unicode ((∘), (≡), (≢), (≤), (≥), (⊥))
 
-
+-- | Short segment, of max length n, that has max sum
 mss ∷ Int → [Integer] → [Integer]
-mss n  = maxWith sum ∘ filter (short n) ∘ segments
+mss n  = maxWith sum ∘ map (msp n) ∘ tails
+
+-- | Short segment, of max length n, that is a prefix and is max sum
+msp ∷ Int → [Integer] → [Integer]
+msp n = maxWith sum ∘ filter (short n) ∘ inits
 
 short ∷ Int → [a] → Bool
 short b xs = (length xs) ≤ b
--- test-suite/Algz/SegmentSpec.hs
+++ test-suite/Algz/SegmentSpec.hs
@@ -7,3 +7,21 @@ spec = do
   describe "short segment with max sum, mss" $ do
     it "run through typical example" $ do
       Algz.Segments.mss 3 [1, -2, 3, 0, -5, 3, -2, 3, -1] `shouldBe` [3,-2,3]
+    it "is empty on a negatives only list" $ do
+      mss 2 [-1,-2,-4,-3] `shouldBe` []
+    it "is sum of list if list is positives" $ do
+      let xs = [4,3,2,8]
+      mss (length xs) xs `shouldBe` xs
+    it "is the max value in the list, for short length of 1, list has one positive" $ do
+      mss 1 [3,-1,4,-2,1,9] `shouldBe` [9]
+
+  describe "prefix with max sum, msp" $ do
+    it "first three are positive, segment max length is five" $ do
+      msp 5 [2,1,3,-2,-1,0] `shouldBe` [2,1,3]
+    it "sums first five despite negatives, segment max length is five" $ do
+      msp 5 [2,-1,3,-2,3,10] `shouldBe` [2,-1,3,-2,3]
+    it "is the head of the list if its positive" $ do
+      let xs = [2,3,4]
+      msp 1 xs `shouldBe` [head xs]
+    it "is empty if the head is negative (for segment max length of 1)" $ do
+      msp 1 [-1,2,-3] `shouldBe` []
