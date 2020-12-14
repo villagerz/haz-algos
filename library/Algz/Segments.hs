c4084300a014107fedc362dd753ddaca56176722
Villager Z <git.villagerz@pm.me>
2020-12-14 14:02:28 -0500

    introduced thinning for msp function

--- library/Algz/Segments.hs
+++ library/Algz/Segments.hs
@@ -1,7 +1,7 @@
 -- | From Chapter 11 "Segments and Subsequences", Algorithm Design with Haskell
 module Algz.Segments where
 import           Data.List       (foldr1, inits, tails)
-import           Prelude.Unicode ((∘), (≡), (≢), (≤), (≥), (⊥))
+import           Prelude.Unicode ((∘), (∧), (≡), (≢), (≤), (≥), (⊥))
 
 -- | Short segment, of max length n, that has max sum
 mss ∷ Int → [Integer] → [Integer]
@@ -9,7 +9,9 @@ mss n  = maxWith sum ∘ map (msp n) ∘ tails
 
 -- | Short segment, of max length n, that is a prefix and is max sum
 msp ∷ Int → [Integer] → [Integer]
-msp n = maxWith sum ∘ filter (short n) ∘ inits
+msp n = maxWith sum ∘ thinBy maxSumPreorder ∘ foldr (op n) [[]]
+  where op b x xss = [] : map (x:) (cut b xss)
+        cut l yss = if length (last yss) ≡ l then (init yss) else yss
 
 short ∷ Int → [a] → Bool
 short b xs = (length xs) ≤ b
@@ -21,3 +23,14 @@ maxWith ∷ Ord b ⇒ (a → b) → [a] → a
 maxWith f  = foldr1 (bigger f)
   where bigger g x y = if g x ≥ g y then x else y
 
+
+maxSumPreorder ∷ (Ord a, Num a) ⇒ [a] → [a] → Bool
+maxSumPreorder xs ys = (sum xs ≥ sum ys) ∧ (length xs ≤ length ys)
+
+thinBy ∷ (a → a → Bool) → [a] → [a]
+thinBy f  = foldr bump []
+  where bump x [] = [x]
+        bump x (y:ys)
+          | f x y = x : ys
+          | f y x = y : ys
+          | otherwise = x : y : ys
--- test-suite/Algz/SegmentSpec.hs
+++ test-suite/Algz/SegmentSpec.hs
@@ -1,10 +1,11 @@
 module Algz.SegmentSpec (spec) where
 import           Algz.Segments
+import           Prelude.Unicode ((∘), (∧), (≡), (≢), (≤), (≥), (⊥))
 import           Test.Hspec
 
 spec ∷ Spec
 spec = do
-  describe "short segment with max sum, mss" $ do
+  describe "mss - short segment with max sum" $ do
     it "run through typical example" $ do
       Algz.Segments.mss 3 [1, -2, 3, 0, -5, 3, -2, 3, -1] `shouldBe` [3,-2,3]
     it "is empty on a negatives only list" $ do
@@ -15,7 +16,7 @@ spec = do
     it "is the max value in the list, for short length of 1, list has one positive" $ do
       mss 1 [3,-1,4,-2,1,9] `shouldBe` [9]
 
-  describe "prefix with max sum, msp" $ do
+  describe "msp - prefix with max sum" $ do
     it "first three are positive, segment max length is five" $ do
       msp 5 [2,1,3,-2,-1,0] `shouldBe` [2,1,3]
     it "sums first five despite negatives, segment max length is five" $ do
@@ -25,3 +26,22 @@ spec = do
       msp 1 xs `shouldBe` [head xs]
     it "is empty if the head is negative (for segment max length of 1)" $ do
       msp 1 [-1,2,-3] `shouldBe` []
+
+  describe "maxSumPreorder - preorde definition to find max sum at smallest length" $ do
+    it "keeps shrter prefix whose sum is at least is big" $ do
+      maxSumPreorder [2,3] [2,3,0,0] `shouldBe` True
+    it "is false even is sum is larger but so is length" $ do
+      maxSumPreorder [3,7] [3] `shouldBe` False
+    it "is false even if length is shorter but sum is smaller" $ do
+      maxSumPreorder [3] [7,2] `shouldBe` False
+    it "is true even if prefixes don't match, just to be clear" $ do
+      maxSumPreorder [5,4] [1,2] `shouldBe` True
+    it "is true is sum is the same and length is the same" $ do
+      maxSumPreorder [2,3] [3,2] `shouldBe` True
+
+  describe "thinBy - for evert ∈ent in xs, there is a y ≤ x" $ do
+    it "finds tuples of coordinates using cmp function" $ do
+      thinBy fcmp [(1,2), (4,3), (2,3), (5,4), (3,1)] `shouldBe` [(1,2), (4,3), (5,4), (3,1)]
+
+fcmp ∷ (Int, Int) → (Int, Int) → Bool
+fcmp (a,b) (c,d) = (a ≥ c) ∧ (b ≤ d)
